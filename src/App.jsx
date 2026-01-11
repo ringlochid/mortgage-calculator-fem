e7fa7c0b8d42bd30a29c9f6309c457a25734b234
Leo Zhang <ring_lochid@outlook.com>
2026-01-11 18:34:13 +1100

    initialize the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Mortgage Repayment Calculator</h1>
+      <p>Start building your React app here!</p>
+    </>
+  )
+}
+
+export default App
